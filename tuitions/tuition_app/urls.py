b6062a3d8f449d26f60687b83d25dad96db1ef76
Eshita <eshitachaskar@gmail.com>
2025-04-02 13:20:45 +0530

    modified:   tuitions/db.sqlite3 	modified:   tuitions/tuition_app/__pycache__/__init__.cpython-312.pyc 	modified:   tuitions/tuition_app/__pycache__/admin.cpython-312.pyc 	modified:   tuitions/tuition_app/__pycache__/apps.cpython-312.pyc 	modified:   tuitions/tuition_app/__pycache__/forms.cpython-312.pyc 	modified:   tuitions/tuition_app/__pycache__/models.cpython-312.pyc 	modified:   tuitions/tuition_app/__pycache__/urls.cpython-312.pyc 	modified:   tuitions/tuition_app/__pycache__/views.cpython-312.pyc 	modified:   tuitions/tuition_app/migrations/__pycache__/0001_initial.cpython-312.pyc 	modified:   tuitions/tuition_app/migrations/__pycache__/0002_tuitionregistration_delete_registration.cpython-312.pyc 	modified:   tuitions/tuition_app/migrations/__pycache__/0003_studentregistration_delete_tuitionregistration.cpython-312.pyc 	new file:   tuitions/tuition_app/migrations/__pycache__/0004_alter_studentregistration_payment_proof.cpython-312.pyc 	modified:   tuitions/tuition_app/migrations/__pycache__/__init__.cpython-312.pyc 	new file:   tuitions/tuition_app/static/tuition_app/imgs/profile-img.png 	modified:   tuitions/tuition_app/templates/tuition_app/filter_students.html 	modified:   tuitions/tuition_app/templates/tuition_app/registration.html 	new file:   tuitions/tuition_app/templates/tuition_app/student_detail.html 	modified:   tuitions/tuition_app/urls.py 	modified:   tuitions/tuition_app/views.py 	modified:   tuitions/tuitions/__pycache__/__init__.cpython-312.pyc 	modified:   tuitions/tuitions/__pycache__/settings.cpython-312.pyc 	modified:   tuitions/tuitions/__pycache__/urls.cpython-312.pyc 	modified:   tuitions/tuitions/__pycache__/wsgi.cpython-312.pyc

--- tuitions/tuition_app/urls.py
+++ tuitions/tuition_app/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='tuition_app/adminlogin.html', redirect_authenticated_user=True), name='login'),
     path('logout/', LogoutView.as_view(next_page='/app/login'), name='logout'),
     path('filter_students/', views.filter_students, name = "filter_students"),
+    path('student/<int:student_id>/', views.student_detail, name="student_detail"),
 ]

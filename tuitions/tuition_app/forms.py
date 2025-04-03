0f65de98d4665fd4d355424a2e91dee30cea6450
Eshita Chaskar <eshitachaskar@gmail.com>
2025-04-04 00:01:16 +0530

    modified:   tuitions/db.sqlite3 	new file:   tuitions/media/student_img/johannes-groll-upXoQv5GAr8-unsplash.jpg 	new file:   tuitions/media/student_img/profile-img.png 	new file:   tuitions/media/student_img/profile-img_BzWpwuW.png 	new file:   tuitions/media/student_img/profile-img_StUvvLy.png 	modified:   tuitions/tuition_app/__pycache__/__init__.cpython-313.pyc 	modified:   tuitions/tuition_app/__pycache__/admin.cpython-313.pyc 	modified:   tuitions/tuition_app/__pycache__/apps.cpython-313.pyc 	modified:   tuitions/tuition_app/__pycache__/forms.cpython-313.pyc 	modified:   tuitions/tuition_app/__pycache__/models.cpython-313.pyc 	modified:   tuitions/tuition_app/__pycache__/urls.cpython-313.pyc 	modified:   tuitions/tuition_app/__pycache__/views.cpython-313.pyc 	modified:   tuitions/tuition_app/forms.py 	modified:   tuitions/tuition_app/migrations/__pycache__/0001_initial.cpython-313.pyc 	modified:   tuitions/tuition_app/migrations/__pycache__/0002_tuitionregistration_delete_registration.cpython-313.pyc 	modified:   tuitions/tuition_app/migrations/__pycache__/0003_studentregistration_delete_tuitionregistration.cpython-313.pyc 	modified:   tuitions/tuition_app/migrations/__pycache__/0004_alter_studentregistration_payment_proof.cpython-313.pyc 	modified:   tuitions/tuition_app/migrations/__pycache__/0005_studentregistration_father_name_and_more.cpython-313.pyc 	modified:   tuitions/tuition_app/migrations/__pycache__/__init__.cpython-313.pyc 	modified:   tuitions/tuition_app/static/tuition_app/js/script.js 	modified:   tuitions/tuition_app/templates/tuition_app/registration.html 	modified:   tuitions/tuition_app/urls.py 	modified:   tuitions/tuition_app/views.py 	modified:   tuitions/tuitions/__pycache__/__init__.cpython-313.pyc 	modified:   tuitions/tuitions/__pycache__/settings.cpython-313.pyc 	modified:   tuitions/tuitions/__pycache__/urls.cpython-313.pyc 	modified:   tuitions/tuitions/__pycache__/wsgi.cpython-313.pyc

--- tuitions/tuition_app/forms.py
+++ tuitions/tuition_app/forms.py
@@ -1,12 +1,24 @@
-# tuition_app/forms.py
 from django import forms
 from .models import StudentRegistration
 
-class StudentRegistrationForm(forms.ModelForm):
+class RegistrationForm(forms.ModelForm):
     class Meta:
         model = StudentRegistration
         fields = '__all__'
         widgets = {
-            'address': forms.Textarea(attrs={'rows': 4}),
-            'fees': forms.TextInput(attrs={'readonly': 'readonly'}),
+            'standard': forms.Select(attrs={'id': 'standard'}),
+            'board': forms.Select(attrs={'id': 'board'}),
+            'subject': forms.Select(attrs={'id': 'subject'}),
+            'school': forms.Select(attrs={'id': 'school'}),
+            'branch': forms.Select(attrs={'id': 'branch'}),
+            'fees': forms.TextInput(attrs={'id': 'fees', 'readonly': True}),
+            'payment_mode': forms.Select(attrs={'id': 'payment_mode'}),
+            'payment_proof': forms.FileInput(attrs={'id': 'payment_proof'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Set initial empty choices for dependent fields
+        self.fields['board'].choices = [('', '-- Select Board --')] + list(self.fields['board'].choices)[1:]
+        self.fields['subject'].choices = [('', '-- Select Subject --')] + list(self.fields['subject'].choices)[1:]
+        self.fields['school'].choices = [('', '-- Select School --')] + list(self.fields['school'].choices)[1:]
